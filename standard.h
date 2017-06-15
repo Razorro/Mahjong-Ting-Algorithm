75485316d098aa652bbb5fab960122216ce11fc6
JuliusBelmont <623571826@qq.com>
2017-06-15 17:24:46 +0800

    重新实现

--- standard.h
+++ standard.h
@@ -3,7 +3,6 @@
 
 // container
 #include <vector>
-#include <array>
 #include <set>
 #include <unordered_set>
 #include <unordered_map>
@@ -24,19 +23,34 @@ typedef unordered_set<int>	hashSet;
 class Analyser
 {
 public:
-	Analyser(int userCard[],int n,int magicCard);
+	enum{ max_value = 0xffff };
+
+	Analyser(int magicCard):analyseCount(0),_magicCardSize(0),_magicCard(magicCard) {}
+	Analyser():analyseCount(0),_magicCardSize(0),_magicCard(0) {}
+	void start(unsigned char userCard[],int n);
 	void printCard(hashMap& userCard) const;
 	inline int getValue(int card) { return card & 0x0f; }
 	inline int getColor(int card){ return (card & 0xf0)>>4; }
+	inline int ChangeToValue(int cbCardIndex)
+	{
+		if (cbCardIndex < 27) return ((cbCardIndex / 9) << 4) | (cbCardIndex % 9 + 1);
+		else return (0x30 | (cbCardIndex - 27 + 1));
+	}
+	bool TingPaiState() { return _tingCard.empty() ? false : true; }
 	hashSet getTingCard() const { return _tingCard; };
 	hashSet getHuCard() const { return _huCard; }
 	bool IsHu() const { return _isHu; }
+	void resetState();
 
 	int analyseCount;
+	int _magicCard;
+	int _magicCardSize;
 
 private:
 	void analyse(hashMap userCard);
+	void analyseWithMagic(hashMap userCard,int magicNum);
 	void dealWithLastCard(hashMap& lastCard,int count);
+	void dealWithLastCard2(hashMap& lastCard,int count,int magicNum);
 	void getSequencialCombo(set<int>& CardValue,hashMap& lastCard);
 	void Solution2(int kindNum,hashMap& lastCard);
 	void Solution3(int kindNum,hashMap& lastCard);
@@ -46,7 +60,4 @@ private:
 	hashSet	_tingCard;
 	hashSet	_huCard;
 	bool _isHu;
-	int _magicCard;
-	int _magicCardSize;
-
 };
