1c944645e655ced7334e0b5a67daa9a470adf3ba
JuliusBelmont <623571826@qq.com>
2017-06-15 17:16:56 +0800

    add test case

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include "standard.h"
 
-#define TEST	arr4
+#define TEST	arr5
 
 int main()
 {
@@ -9,6 +9,7 @@ int main()
 	int arr2[] = {1,2,4,4,5,6,9,9};
 	int arr3[] = {1,2,3,3,4,5,24,24,34,34,31};
 	int arr4[] = {1,1,1,2,2,2,3,3,3,5,7,8,9,9};
+	int arr5[] = { 0x7,0x8,0x9,0x11,0x12,0x13,0x21,0x22,0x23,0x27,0x29,0x29,0x31,0x31 };
 
  	int s = distance(begin(TEST),end(TEST));
 	auto a = Analyser(TEST,s,0);
